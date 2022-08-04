097194f794e7add7bac69242af793244a1527757
Ngoni Mapfumo <ngonimapfumo@hotmail.com>
2022-08-04 20:51:37 +0200

    add movie model

---
+++ app/src/main/java/zw/co/nm/movieskt/data/models/Movie.kt
@@ -0,0 +1,10 @@
+package zw.co.nm.movieskt.data.models
+
+data class Movie (
+        private var medium_cover_image: String,
+        private val large_cover_image: String,
+        private val title: String,
+        private val year: String,
+        private val language: String,
+        private val genres: List<*>
+        )
